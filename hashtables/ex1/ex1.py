030ba93c73b07da9befc49aeb9fc8bf772d89eca
adxpillar <dsa.adeagbo@gmail.com>
2020-07-10 13:38:03 -0400

    first commit with two exercises

--- hashtables/ex1/ex1.py
+++ hashtables/ex1/ex1.py
@@ -3,5 +3,15 @@ def get_indices_of_item_weights(weights, length, limit):
     YOUR CODE HERE
     """
     # Your code here
-
+    # create hash table
+    index = {}
+    # enumerate weights 
+    for key,value in enumerate(weights):
+        # conditional
+        wb = limit - value
+        if wb not in index:
+            # swap value for list index
+            index[value] = key
+        else:
+            return key,index[wb]
     return None
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,6 +1,17 @@
 # Your code here
+import urllib.request
+import datetime
+
+CACHE_EXPIRATION_SECONDS = 10
 
 
+cache = {}
+
+def get_timestamp():
+    return datetime.datetime.now().timestamp()
+
+
+timestamp = get_timestamp()
 
 def finder(files, queries):
     """
@@ -8,6 +19,18 @@ def finder(files, queries):
     """
     # Your code here
 
+    cur_time = get_timestamp()
+
+    for path in files:
+        if (path not in cache) or (cur_time - cache[path].timestamp > CACHE_EXPIRATION_SECONDS):
+            print("cache miss!")
+            resp = urllib.request.urlopen(path)
+
+            data = resp.read()
+            resp.close()
+            data = data.decode()
+            # cache[path] = CacheEntry(path,data)
+            result = cache[path].data
     return result
 
 
@@ -23,3 +46,6 @@ if __name__ == "__main__":
         "baz"
     ]
     print(finder(files, queries))
+
+
+
